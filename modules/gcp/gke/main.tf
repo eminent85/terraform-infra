64f5cd2824f9a0362a25c1d9a372c117c6609456
Justin Beeman <ssj4gokou85@hotmail.com>
2025-11-06 11:31:22 -0800

    update actions

--- modules/gcp/gke/main.tf
+++ modules/gcp/gke/main.tf
@@ -4,6 +4,8 @@ resource "google_container_cluster" "primary" {
   location = var.regional ? var.region : var.zone
   project  = var.project_id
 
+  deletion_protection = false
+
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
